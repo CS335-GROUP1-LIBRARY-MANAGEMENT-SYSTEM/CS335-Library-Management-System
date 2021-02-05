3d05a29abf407739e379f37a9f15263880f460f2
robertanold36 <robertanold70@gmail.com>
2021-02-05 21:34:58 +0100

    creating controller for the booking and registering of books

--- src/main/java/com/example/librarysystem/controller/BookingController.java
+++ src/main/java/com/example/librarysystem/controller/BookingController.java
@@ -15,5 +15,40 @@ import java.util.List;
 @AllArgsConstructor
 public class BookingController {
 
+    private final BookService bookService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Void> saveBook(@RequestBody BookDto bookDto){
+        bookService.saveBook(bookDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAllBooks")
+    public ResponseEntity<List<BookDto>> getAllBooks()
+    {
+        List<BookDto> books = bookService.getBooks();
+        return new ResponseEntity<>(books,HttpStatus.OK);
+
+    }
+
+    @PostMapping("/booking")
+    public ResponseEntity<Void> booking(@RequestBody BookingDto bookingDto){
+        bookService.booking(bookingDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getBook/{id}")
+    public ResponseEntity<BookDto> getBook(@PathVariable Long id){
+        BookDto book = bookService.getBook(id);
+        return new ResponseEntity<>(book,HttpStatus.OK);
+    }
+
+    @PutMapping("/editBoo/{id}")
+    public  ResponseEntity<Void> editBook(@PathVariable Long id){
+        bookService.editBook(id);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
 
 }
