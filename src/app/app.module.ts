91a4d2967b490d0544ff631c608e0b4d313342d2
tiffahm <51071990+tiffahm@users.noreply.github.com>
2021-02-07 21:50:01 +0300

    adding registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatCardModule} from '@angular/material/card';
-import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import {RouterModule, Routes} from '@angular/router';
-import {MatListModule} from '@angular/material/list';
-import {MatTreeModule} from '@angular/material/tree';
-import { UserInfoComponent } from './user-dashboard/user-info/user-info.component';
-import { UserBooksComponent } from './user-dashboard/user-books/user-books.component';
-import { UserPaymentsComponent } from './user-dashboard/user-payments/user-payments.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { RegistrationComponent } from './registration/registration.component';
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./user-dashboard/user.module').then(mod => mod.UserModule)}
+  {path: '', component: RegistrationComponent},
+  {path: 'user', loadChildren: () => import('./user-dashboard/user.module').then(mod => mod.UserModule)},
+  {path: '**', component: RegistrationComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatButtonModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
