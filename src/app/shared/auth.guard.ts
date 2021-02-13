b5141562ad1adb2e3321a56f86fa925894180466
MARYAM <maryamamin616@gmail.com>
2021-02-13 07:08:25 +0300

    Add a guard when navigating through pages using URL This code will restrict users to access other pages using the URL unless they have a valid authentication token based on their role.

---
+++ src/app/shared/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from './auth.service';
+import {LocalStorageService} from 'ngx-webstorage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService:AuthService,
+    private router:Router,
+    private localStorageService:LocalStorageService) {
+}
+ 
+canActivate(
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  let url:string=state.url;
+  const  isAuthenticated:boolean=this.authService.isLoggedIn();
+
+  if(isAuthenticated){
+    return true;
+  }else {
+    this.localStorageService.store('returnUrl',url);
+    this.router.navigate(["/login"]);
+    return false;
+  }
+}
+
+
+  
+}
