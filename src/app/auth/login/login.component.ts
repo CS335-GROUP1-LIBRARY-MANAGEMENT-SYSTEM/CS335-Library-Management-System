9d3093fd59a7d4a897c95d3689e7dc0cc5916479
MARYAM <maryamamin616@gmail.com>
2021-02-13 07:05:31 +0300

    Add validation and conditions for inputs in the login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../shared/auth.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {LocalStorageService} from 'ngx-webstorage';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+ 
+  loginForm:FormGroup;
+  isError!:boolean;
+
+
+
+  constructor(private formBuilder: FormBuilder
+              ,private authService:AuthService,private router:Router,
+              private toastr:ToastrService,private activeRoute:ActivatedRoute,
+              private localStorageService:LocalStorageService) { }
 
   ngOnInit(): void {
+
+    this.loginForm=this.formBuilder.group({
+      username:['',Validators.required],
+      password:['',Validators.required]
+    })
   }
+
+  get f(){
+    return(this.loginForm.controls);
+  }
+
+  onSubmit(){
+    this.authService.login(this.loginForm.value).subscribe(loginResponse=>{
+
+      if (loginResponse) {
+        this.isError = false;
+        let url = this.localStorageService.retrieve('returnUrl')
+        if (url) {
+          console.log('we are here ');
+          this.router.navigate([url]);
+          this.toastr.success('login successfull');
+        }else {
+          this.router.navigate(['/user']);
+          this.toastr.success('login successfull');
+        }
+
+      }
+    },()=>{
+      this.isError=true;
+      //this.toastr.error("wrong username and password");
+    })
+  }
+
 
 }
