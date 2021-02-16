bfbfced63dd8acefbfbe212caaca6583bb215903
raksbaliwena <rakim_omar41@hotmail.com>
2021-02-16 16:33:57 +0300

    modify admin dashborad components

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -54,7 +54,7 @@ interface CheckFlatNode {
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
-  role:string;
+  username:string;
   opened: boolean;
   private _transformer = (node: FoodNode, level: number) => {
     return {
@@ -74,7 +74,7 @@ export class AdminDashboardComponent implements OnInit {
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
   constructor(private localStorage:LocalStorageService, private router:Router) {
     this.dataSource.data = TREE_DATA;
-    this.role=this.localStorage.retrieve('role');
+    this.username=this.localStorage.retrieve('username');
   }
   hasChild = (_: number, node: CheckFlatNode) => node.expandable;
   ngOnInit(): void {
